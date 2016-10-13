f8792fe81410941fb43ec22ea13f6f4e85ab3a75
jenboyd <jboyd6290@gmail.com>
2016-10-13 13:53:55 -0400

    Add Readfile function to s3-upload.js

---
+++ bin/s3-upload.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+
+const filename = process.argv[2] || '';
+
+const readFile = (filename) => {
+  fs.readFile(filename, (error, data) => {
+    if (error) {
+      return console.error(error);
+    }
+
+    console.log(`${filename} is ${data.length} bytes long`);
+  });
+};
