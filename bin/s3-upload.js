8a3f588e1df0ab291e20d87144d8d93d1afbe4d8
jenboyd <jboyd6290@gmail.com>
2016-10-13 15:41:10 -0400

    Prepare AWS S3 options
    Define the bucket name

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -31,12 +31,31 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
-const logMessage = (file) => {
-  console.log(`${filename} is ${file.data.length} bytes long and is of mimeType ${file.mime}`);
+const upload = (file) => {
+  const options = {
+    //get bucket name from AWS console
+    Bucket: 'jenniferboyd',
+    //attach the fileBuffer as a stream to send to Amazon.
+    Body: file.data,
+    //allow anyone to access the URL of the uploaded file
+    ACL: 'public-read',
+    //tell s3 what the mime-type is
+    contentType: file.mime,
+    //pick a filename for S3 to use for the upload
+    key: `test/test.$file.ext`
+  };
+  return Promise.resolve(options);
+};
+
+//don't actually upload yet, just pass the data down the Promise chain
+const logMessage = (upload) => {
+  delete upload.body;
+  console.log(`the upload options are ${JSON.stringify(upload)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(upload)
 .then(logMessage)
 .catch(console.error)
 ;
