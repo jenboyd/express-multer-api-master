d4f85cee27cd9e9fb18bf79fa293f2bc24483d79
jenboyd <jboyd6290@gmail.com>
2016-10-14 11:48:05 -0400

    Create filenames with low change of duplication.
    Create functions to add random hex file name.
    
    Add data as directory name.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,9 +1,11 @@
 'use strict';
 
-//this had to come before anything else.
+// this has to come before anything else
 require('dotenv').config();
 
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
 const AWS = require('aws-sdk');
 
@@ -15,13 +17,14 @@ const readFile = (filename) => {
       if (error) {
         reject(error);
       }
+
       resolve(data);
     });
   });
 };
 
-//return a default object in the case fileType is given an unsupported.
-//filetype to read.
+// return a default object in the case that fileType is given an unsupported
+// filetype to read
 const mimeType = (data) => {
   return Object.assign({
     ext: 'bin',
@@ -35,6 +38,31 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
+};
+
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
 const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -44,37 +72,38 @@ const s3 = new AWS.S3({
 
 const upload = (file) => {
   const options = {
-    //get bucket name from AWS console
+    // get the bucket name from your AWS S3 console
     Bucket: 'jenniferboyd',
-    //attach the fileBuffer as a stream to send to Amazon.
+    // attach the fileBuffer as a stream to send to S3
     Body: file.data,
-    //allow anyone to access the URL of the uploaded file
+    // allow anyone to access the URL of the uploaded file
     ACL: 'public-read',
-    //tell s3 what the mime-type is
+    // tell S3 what the mime-type is
     ContentType: file.mime,
-    //pick a filename for S3 to use for the upload
-    Key: `test/test.${file.ext}`
+    // pick a filename for S3 to use for the upload
+    Key: `${file.dir}/${file.name}.${file.ext}`
   };
+
   return new Promise((resolve, reject) => {
     s3.upload(options, (error, data) => {
       if (error) {
         reject(error);
       }
-      resolve (data);
+
+      resolve(data);
     });
   });
 };
 
-
-
-//don't actually upload yet, just pass the data down the Promise chain
 const logMessage = (response) => {
-  delete upload.body;
-  console.log(`response from AWS was options are ${response}`);
+  // turn the pojo into a string so I can see it on the console
+  console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
 .then(logMessage)
 .catch(console.error)
