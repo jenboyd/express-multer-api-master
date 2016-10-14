1be1785e37634f569c2eeec5353af46b06ee083f
jenboyd <jboyd6290@gmail.com>
2016-10-14 14:02:16 -0400

    Add upload model

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 
 const upload = require('../lib/s3-upload').upload;
 
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
+
 const filename = process.argv[2] || '';
+const comment = process.argv[3] || 'No comment';
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -18,13 +22,23 @@ const readFile = (filename) => {
   });
 };
 
-const logMessage = (response) => {
+const createUpload = (response) => {
+  let upload = {
+    location:response.Location,
+    comment: comment,
+  };
+  return Upload.create(upload);
+};
+
+const logMessage = (upload) => {
   // turn the pojo into a string so I can see it on the console
-  console.log(`the response from AWS was ${JSON.stringify(response)}`);
+  console.log(`${JSON.stringify(upload)}`);
 };
 
 readFile(filename)
 .then(upload)
+.then(createUpload)
 .then(logMessage)
 .catch(console.error)
+.then(() => mongoose.connection.close())
 ;
